90248b9a391cb5c6174ff707c4e982c388afe213
DavidRMorphew <morphewd@umich.edu>
2020-10-14 19:29:01 -0400

    testing if .env is hidden for API-Key

--- environment.rb
+++ environment.rb
@@ -2,6 +2,9 @@ require_relative "./lib/api"
 require_relative "./lib/book"
 require_relative "./lib/cli"
 
+require 'dotenv/load'
+# Follow up on this to hide SSH key from being uploaded to Github and becoming accessible
+# 
 require 'pry'
 require 'net/http'
 require 'json'
