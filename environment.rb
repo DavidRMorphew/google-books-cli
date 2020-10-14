9b93d6a55365de3b68abc09dafe1eba248f112c8
DavidRMorphew <morphewd@umich.edu>
2020-10-14 14:32:12 -0400

    first commit with environment setup and initial test of run passed

---
+++ environment.rb
@@ -0,0 +1,10 @@
+require_relative "./lib/api"
+require_relative "./lib/author"
+require_relative "./lib/book"
+require_relative "./lib/cli"
+
+require 'pry'
+require 'net/http'
+require 'json'
+# Require awesome-print gem here after you get things running. It's a flourish, not essential.
+
