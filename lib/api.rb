aebde971708c053749b835a39a783771910393e1
DavidRMorphew <morphewd@umich.edu>
2020-10-15 16:49:57 -0400

    API fetch test passed; book_array created

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,11 @@
 class API
-    
+    def self.fetch_free_books
+        url = "https://www.googleapis.com/books/v1/volumes?q=filter=free-ebooks&Pagination=maxResults&printType=books"
+        
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        hash = JSON.parse(response)
+        binding.pry
+        array_of_books = hash["items"]
+    end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,5 +2,6 @@ class CLI
 
     def start
         puts "Welcome"
+        API.fetch_free_books
     end
 end
