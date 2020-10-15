eae9d2c74342ac6781dbdbbf9287136f580aed23
DavidRMorphew <morphewd@umich.edu>
2020-10-15 17:23:25 -0400

    url search fixed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Google Books CLI
+
+This CLI grabs the top forty books that are free to download from Google Books.
+
--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,18 @@
 class API
     def self.fetch_free_books
-        url = "https://www.googleapis.com/books/v1/volumes?q=filter=free-ebooks&Pagination=maxResults&printType=books"
-        
+        url = "https://www.googleapis.com/books/v1/volumes?q=programming&download=epub&filter=free-ebooks&filter=full&Pagination=maxResults&printType=books"
+        binding.pry
         uri = URI(url)
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
-        binding.pry
+        
         array_of_books = hash["items"]
+
+        # title
+        # author
+        # description
+        # ISBN
+        # categories [=genre]
+        # filter for acsTokenLink - eliminates books not in free pdf format
     end
 end
