7c6ba4e66932f357ab7b756e850c56f22fbc568e
DavidRMorphew <morphewd@umich.edu>
2020-10-15 20:50:10 -0400

    book instances created with attributes

--- lib/api.rb
+++ lib/api.rb
@@ -5,15 +5,25 @@ class API
         uri = URI(url)
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
-        binding.pry
         array_of_books = hash["items"]
-        books_array_with_free_pdf = array_of_books.select {|book_hash| book_hash["accessInfo"]["pdf"]["acsTokenLink"]}
+
+        array_of_books.each do |book_hash|
+            binding.pry
+            book = Book.new
+            book.title = book_hash["volumeInfo"]["title"]
+            book.subtitle = 
+            book_hash["volumeInfo"]["author"] ? book.author = book_hash["volumeInfo"]["author"] : book.author = book_hash["volumeInfo"]["authors"]
+            book.pdf_link = book_hash["accessInfo"]["pdf"]["acsTokenLink"]
+            book.publication_date = book_hash["volumeInfo"]["publishedDate"]
+            book.description = book_hash["volumeInfo"]["description"]
+            book.categories = book_hash["volumeInfo"]["categories"]
+        end
         
         # title & subtitle
         # author
-        # year
+        # publication year
         # description
-        # ISBN
+        # ISBN?
         # categories [=genre]
         # filter for acsTokenLink - eliminates books not in free pdf format
         # array_of_books[0]["accessInfo"]["pdf"]["acsTokenLink"]
--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,3 @@
 class Book
-    
+    attr_accessor :title, :subtitle, :author, :pdf_link, :publication_date, :description, :categories
 end
