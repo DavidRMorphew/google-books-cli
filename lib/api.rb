f1887356d735f46e03251e3050d3945d1714d143
DavidRMorphew <morphewd@umich.edu>
2020-10-16 06:52:15 -0400

    url refined & book attributes updated

--- lib/api.rb
+++ lib/api.rb
@@ -1,22 +1,27 @@
 class API
     def self.fetch_free_books
-        url = "https://www.googleapis.com/books/v1/volumes?q=programming&download=epub&filter=free-ebooks&filter=full&printType=books&maxResults=40"
+        url = "https://www.googleapis.com/books/v1/volumes?q=object+programming&download=epub&filter=free-ebooks&filter=full&printType=books&maxResults=40"
         
         uri = URI(url)
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
         array_of_books = hash["items"]
-
         array_of_books.each do |book_hash|
-            binding.pry
             book = Book.new
             book.title = book_hash["volumeInfo"]["title"]
-            book.subtitle = 
-            book_hash["volumeInfo"]["author"] ? book.author = book_hash["volumeInfo"]["author"] : book.author = book_hash["volumeInfo"]["authors"]
+            book.subtitle = book_hash["volumeInfo"]["subtitle"]
+            book.author = book_hash["volumeInfo"]["authors"]
             book.pdf_link = book_hash["accessInfo"]["pdf"]["acsTokenLink"]
             book.publication_date = book_hash["volumeInfo"]["publishedDate"]
             book.description = book_hash["volumeInfo"]["description"]
             book.categories = book_hash["volumeInfo"]["categories"]
+            if book_hash["volumeInfo"]["industryIdentifiers"]
+                book.isbn_nums = book_hash["volumeInfo"]["industryIdentifiers"].collect do |isbn_hash| 
+                    "#{isbn_hash["type"].gsub("_"," ")}: #{isbn_hash["identifier"]}"
+                end
+            else
+                book.isbn_nums = nil
+            end
         end
         
         # title & subtitle
--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,11 @@
 class Book
-    attr_accessor :title, :subtitle, :author, :pdf_link, :publication_date, :description, :categories
+    attr_accessor :title, :subtitle, :author, :pdf_link, :publication_date, :description, :categories, :isbn_nums
+    @@all = []
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
