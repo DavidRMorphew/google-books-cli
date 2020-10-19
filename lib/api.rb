705ae1851c60add2a3cd40b135104c20670c6d92
DavidRMorphew <morphewd@umich.edu>
2020-10-19 18:07:14 -0400

    author names parsed in API fetch

--- lib/api.rb
+++ lib/api.rb
@@ -8,16 +8,17 @@ class API
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
         array_of_books = hash["items"]
-        array_of_books.each do |book_hash|
-                   
+        array_of_books.each do |book_hash|   
             assignment_hash = {}
-            assignment_hash[:authors] = book_hash["volumeInfo"]["authors"]
+            assignment_hash[:authors] = book_hash["volumeInfo"]["authors"].map {|name| name.split(" ").map {|name_part| name_part.capitalize}.join(" ")}
             assignment_hash[:title] = book_hash["volumeInfo"]["title"]
             assignment_hash[:subtitle] = book_hash["volumeInfo"]["subtitle"]
             assignment_hash[:description] = book_hash["volumeInfo"]["description"]
             assignment_hash[:publication_date] = book_hash["volumeInfo"]["publishedDate"]
-                pdf_link = book_hash["accessInfo"]["pdf"]["acsTokenLink"]
-                epub_link = book_hash["accessInfo"]["epub"]["acsTokenLink"]
+                
+            pdf_link = book_hash["accessInfo"]["pdf"]["acsTokenLink"]
+            epub_link = book_hash["accessInfo"]["epub"]["acsTokenLink"]
+            
             assignment_hash[:link] = [pdf_link, epub_link]
             
             if book_hash["volumeInfo"]["industryIdentifiers"]
