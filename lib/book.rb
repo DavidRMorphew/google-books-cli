5ed2c0ffee6b1aa5e9ed96ae16eaa69766f9fa92
DavidRMorphew <morphewd@umich.edu>
2020-10-21 10:56:48 -0400

    CLI#book_list_by_title created

--- lib/book.rb
+++ lib/book.rb
@@ -13,8 +13,9 @@ class Book
     def self.find_by_language(language)
         Book.all.select {|book| book.languages.include?(language) if book.languages}
     end
+    # ask about using another level of .select rather than if logic
 
-    def self.only_on_computers
+    def self.only_with_computers_category
         Book.all.select do |book| 
             book.categories.any? {|category| category.match?(/Computer/)} if book.categories
         end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,27 @@
 class CLI
-
     def start
+        puts "Welcome!"
         API.fetch_free_books
+        # binding.pry
+        self.book_list_by_title
+    end
+
+    def book_list_by_title
+        # give user the option to see a book list by title
+        # list out the books by their titles and subtitles, if there are subtitles
+        puts "Would you like to see a list of books by their titles?"
+        puts "Type 'yes' to see the book list or any other key to exit."
+        
+        user_input = gets.strip.downcase
+        # if the user says yes, provide a book list by titles
+        if user_input == "yes" || user_input == "y"
+            puts "Enjoy browsing by title!"
+            # display the book list by titles
+            self.display_books_by_title
+        end
+    end
+
+    def display_books_by_title
         binding.pry
     end
 end
