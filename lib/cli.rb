8cce0549035de180c3dadc39daf7c4b25746e7c9
DavidRMorphew <morphewd@umich.edu>
2020-10-21 13:38:17 -0400

    CLI#display_book_details created; @ask_count added

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,8 @@
 class CLI
+    @ask_count = 0
+
     def start
-        puts "Welcome!"
+        puts "\nWelcome!"
         API.fetch_free_books
         # binding.pry
         self.book_list_by_title
@@ -9,8 +11,8 @@ class CLI
     def book_list_by_title
         # give user the option to see a book list by title
         # list out the books by their titles and subtitles, if there are subtitles
-        puts "Would you like to see a list of books by title?"
-        puts "Type 'yes' to see the book list or any other key to exit."
+        puts "\nWould you like to see a list of books by title?"
+        puts " Type 'yes' to see the book list or any other key to exit."
         
         user_input = gets.strip.downcase
         # if the user says yes, provide a book list by titles
@@ -34,26 +36,32 @@ class CLI
     # def display_books_by_attribute(attribute)
     #     Book.all.each.with_index(1) do |book, index|    
     #         puts "(#{index}) " + book.send("#{attribute}")
-    #         # if index % 10 == 0
-    #         #     sleep(2)
-    #         #     puts "\n"
-    #         # end
     #     end
     # end
 
     def ask_user_for_book_choice
-        puts "Enter the number of a book for more information."
-        puts "To exit, type 'exit'."
+        if @ask_count == 0
+            puts "\nEnter the number of a book for more information."
+            puts "   To exit, type 'exit'."
+            @ask_count += 1
+        end
+        
         user_input = gets.strip
         index = user_input.to_i - 1
-        binding.pry
-        if index.between?(1,40)
-            Drink.all[index]
+
+        if index.between?(0,Book.all.length - 1)
+            selected_book_instance = Book.all[index]
+            display_book_details(selected_book_instance)
         elsif user_input.downcase == 'exit'
             puts "Goodbye!"
         else
-            puts "Please enter a number between 1 and 40 for more information on a book" 
+            puts "Please enter a number between 1 and #{Book.all.length} for more information on a book" 
             puts "Or type 'exit' to exit."
+            ask_user_for_book_choice
         end
+    end
+
+    def display_book_details(book)
+        puts "Title: '#{book.title}. #{book.subtitle if book.subtitle}'"
     end
 end
