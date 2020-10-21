6289e417b354397eb70f21bf0a53b7f1ca21ad26
DavidRMorphew <morphewd@umich.edu>
2020-10-21 11:33:56 -0400

    CLI#display_books_by_title better output

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,7 +15,8 @@ class CLI
         user_input = gets.strip.downcase
         # if the user says yes, provide a book list by titles
         if user_input == "yes" || user_input == "y"
-            puts "Enjoy browsing by title!"
+            puts "\nEnjoy browsing by title!\n\n"
+            sleep(1)
             # display the book list by titles
             self.display_books_by_title
         end
@@ -23,7 +24,15 @@ class CLI
 
     def display_books_by_title
         Book.all.each.with_index(1) do |book, index|    
-            puts "(#{index}.) #{book.title}" 
+            puts "(#{index}) #{book.title}"
+            if index % 10 == 0
+                sleep(2)
+                puts "\n"
+            end
         end
     end
+
+    # def display_books_by_attribute(attribute)
+
+    # end
 end
